72e41786ad8b673dd08f4ed98c7a52f01dd9292f
Mohamad Salimi <mohamad.salimi@hotmail.com>
2021-11-25 13:38:27 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+let check = document.querySelector("#check");
+let basic = document.querySelector("#basic");
+let professional = document.querySelector("#professional");
+let master = document.querySelector("#master");
 
+basic.innerHTML = "19.99";
+professional.innerHTML = "24.99";
+master.innerHTML = "39.99";
+
+check.addEventListener("click", function (e) {
+    if (check.checked == true) {
+        basic.innerHTML = "199.99";
+        professional.innerHTML = "249.99";
+        master.innerHTML = "399.99";
+    } else {
+        basic.innerHTML = "19.99";
+        professional.innerHTML = "24.99";
+        master.innerHTML = "39.99";
+    }
+});
